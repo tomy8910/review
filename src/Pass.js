182228b620b499c7b0c4d13377572f9f1fbd47ce
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2017-07-11 09:39:37 +0800

    haha

--- src/Pass.js
+++ src/Pass.js
@@ -13,6 +13,7 @@ const RandomPic = styled.img`
   position: absolute;
   top: ${props => props.top};
   left: ${props => props.left};
+  border-radius: ${props => props.radius};
 `
 
 export default class Pass extends Component {
@@ -27,10 +28,11 @@ export default class Pass extends Component {
   }
 
   destroyWorld() {
-    const left = Math.floor(Math.random() * 100)
-    const top = Math.floor(Math.random() * 100)
+    const left = Math.floor(Math.random() * 70)
+    const top = Math.floor(Math.random() * 70)
+    const radius = Math.floor(Math.random() * 10 + 1)
     const rand = [...this.state.rand]
-    rand.push([left, top])
+    rand.push([left, top, rand])
     this.setState({
       imgs: (this.state.imgs += 1),
       rand
@@ -56,6 +58,7 @@ export default class Pass extends Component {
               src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.0-9/15284096_1297887133566925_4777882303825093241_n.jpg?oh=6ed4a12e310a2b562e4fd38cc34ee290&oe=5A0038E2"
               top={`${this.state.rand[i][0]}%`}
               left={`${this.state.rand[i][1]}%`}
+              radius={`${this.state.rand[i][2]}px`}
             />
           )
         })}
